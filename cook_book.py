9abf224c708bb5bb734bdc28751f56df09f78743
Maxim Kocheshkov <gror_godfroy@mail.ru>
2020-04-05 22:32:44 +0300

    Задача 2

--- cook_book.py
+++ cook_book.py
@@ -18,4 +18,57 @@ print('cook_book = ')
 for dishes, consist_list in cook_book.items():
   print(f'{dishes} : \n {consist_list}')
 
+# Task 2
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+      for ingridient in cook_book[dish]:
+        new_shop_list_item = dict(ingridient)
+        key = new_shop_list_item['ingredient_name']
+        list_data = []
+        availability_key = shop_list.get(key)
+        if availability_key  == None:
+          list_data.append({'measure': new_shop_list_item['measure'], 'quantity':int(new_shop_list_item['quantity'])* person_count})
+          shop_list[key] = list_data
+        else:
+           shop_list[key][0]["quantity"] += int(new_shop_list_item['quantity'])* person_count
+
+    print(shop_list)
+          
+def in_dictionary(key, dict):
+    if key in dict:
+        return True
+    return False
   
+def cooking(cook_book):
+  menu = input('Введите название блюд через запятую: ')
+  menu_structure = menu.strip().split(',')
+  quantity_error = True
+  for i in range(len(menu_structure)):
+    if in_dictionary(menu_structure[i], cook_book) == False:
+       print(f'{menu_structure[i]} - такого блюда в меню нет ')
+       quantity_error = False
+  if quantity_error == True:
+    quantity_people = int(input('Введите количество персон: '))
+    if quantity_people == 1:
+      print(f'Ингредиенты на {quantity_people} персону')
+    elif 1 < quantity_people < 5:
+      print(f'Ингредиенты на {quantity_people} персоны')
+    else:
+      print(f'Ингредиенты на {quantity_people} персон')
+    get_shop_list_by_dishes(menu_structure, int(quantity_people))
+
+
+def main():
+  print('Введите команду: ')
+  while True:
+    user_input = input()
+    if user_input == 'o':
+      open_file(cook_book)
+    elif user_input == 'm':
+      cooking(cook_book)
+    elif user_input == 'q':
+      print('До свидания!')
+      break
+
+main()  
